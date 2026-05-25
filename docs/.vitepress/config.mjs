eeeedffe727ace1d5c6bf5f08d76d4a749301bef
ewald <ewald.hild@web.de>
2026-05-25 14:56:27 +0200

    feat: update documentation to include new production setup

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -37,6 +37,10 @@ export default withMermaid(defineConfig({
     ]
   },
   themeConfig: {
+    outline: {
+      level: [2, 3],
+      label: 'On this page',
+    },
     search: {
       provider: "local",
     },
